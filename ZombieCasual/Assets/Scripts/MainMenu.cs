427559ff39919b51bd63fe2fbd03dc54c5bf1b86
GabrielKepecs <gabkep@outlook.com>
2022-04-25 11:17:09 -0300

    Menu de Morte + Loading finalizados;

--- ZombieCasual/Assets/Scripts/MainMenu.cs
+++ ZombieCasual/Assets/Scripts/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("OK");
     }
 
     public void QuitGame()
@@ -19,5 +20,10 @@ public class MainMenu : MonoBehaviour
     public void PlayMenu()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
 }
